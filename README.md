d00f10a3929fb5ff070004e8463c269a2eef659d
Ana Constantinescu <constantinescuana2003@gmail.com>
2020-02-05 16:24:18 +0200

    refactoring to edge

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # nodejsapp
-Making a nodejs app working with MongoDB
+Making a nodejs app working with MongoDB. Under there will be all modules stat need to be installed
+1. Step 1 (Init)
+	- npm init
+	- npm install express
+	- npm install nodemon --save-dev  // to run nodemon in developing mode so you will not restart your prject every time you make a modifiction
+	- npm install express-edge  // So you can face a footer and a header template for all pages
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // ----- Imports
 const express = require('express')
 const path = require('path')
+const expressEdge = require('express-edge')
 
 // ----- Declarations 
 const app = new express()
@@ -8,12 +9,25 @@ const app = new express()
 
 // ----- Use
 app.use(express.static('public'))
+app.use(expressEdge.engine)
+
+// ----- Set
+app.set('views', `${__dirname}/views`)
 
 // ----- Get
 app.get('/', (request, response) => {
-	response.sendFile(path.resolve(__dirname, 'pages/index.html'))
+//	response.sendFile(path.resolve(__dirname, 'pages/index.html'))
+	response.render('index')
 })
-
+app.get('/about', (request, response) => {
+	response.render('about')
+})
+app.get('/contact', (request, response) => {
+	response.render('contact')
+})
+app.get('/post', (request, response) => {
+	response.render('post')
+})
 
 app.listen(4000, () => {
 	console.log("App is listening at port 4000")
